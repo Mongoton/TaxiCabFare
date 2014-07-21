77ee61d8e6bc314fddbd5136d19170a0f9456ecd
Ben Ng <benning25@gmail.com>
2014-07-21 15:31:05 -0400

    Commit1

---
+++ TaxiCabFare/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaxiCabFare.Models;
+
+
+namespace TaxiCabFare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(TaxiFareModel rideinfo)
+        {
+            string result = "";
+            try
+            {
+                int above6min = Int32.Parse(rideinfo.TaxiAbove6Min);
+                int below6min = Int32.Parse(rideinfo.TaxiBelow6Min);
+                DateTime taxidate = DateTime.Parse(rideinfo.RideDate);
+            }
+            catch (Exception e)
+            {
+                result = "Please enter valid values";
+            }
+            return Json(new { result });
+        }
+    }
+}
---
+++ TaxiCabFare/Scripts/taxifarectrl.js
@@ -0,0 +1,16 @@
+﻿app.controller("MainController", function ($scope, $http) {
+    $scope.calculate = function (ride) {
+        var result = $http.post("/Home/Index/", ride);
+        //alert("heretest1");
+        result.success(function (data) {
+            //alert("heretest2");
+            $scope.totalfare = "The total fare is: " + data.result;
+            //alert("heretest3");
+        });
+        //alert('hiho');
+    };
+    //$scope.message = 'blah';
+    //        calculate.get().then(function (rideInfo) { $scope.rideInfo = rideInfo });
+    //$scope.calculate = function (ride) {
+    //};
+});
